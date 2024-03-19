c88b9fe738f3501061d742eb47bb4e49719829f6
dttung79 <dttung@gmail.com>
2024-03-19 09:43:17 +0700

    update book.py

--- week7/book.py
+++ week7/book.py
@@ -19,4 +19,22 @@ class Book:
             raise ValueError('Title cannot be empty')
         self.__title = value
 
+    @property
+    def author(self):
+        return self.__author
     
+    @author.setter
+    def author(self, value):
+        if value == '':
+            raise ValueError('Author cannot be empty')
+        self.__author = value
+    
+    @property
+    def price(self):
+        return self.__price
+    
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError('Price cannot be negative')
+        self.__price = value
